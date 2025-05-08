c8cb1aa422d2ab57dcdc4a85cd1ee5de70a41355
Luciana Diaz <124196373+Lucessy@users.noreply.github.com>
2025-05-08 22:41:29 +0000

    Implementación de tabla con nombre del fichero y pixeles

--- models.py
+++ models.py
@@ -1,32 +1,17 @@
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import validates
+from datetime import datetime
 
 from app import db
 
-
-class Restaurant(db.Model):
-    __tablename__ = 'restaurant'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    street_address = Column(String(50))
-    description = Column(String(250))
-
-    def __str__(self):
-        return self.name
-
-class Review(db.Model):
-    __tablename__ = 'review'
-    id = Column(Integer, primary_key=True)
-    restaurant = Column(Integer, ForeignKey('restaurant.id', ondelete="CASCADE"))
-    user_name = Column(String(30))
-    rating = Column(Integer)
-    review_text = Column(String(500))
-    review_date = Column(DateTime)
-
-    @validates('rating')
-    def validate_rating(self, key, value):
-        assert value is None or (1 <= value <= 5)
-        return value
-
-    def __str__(self):
-        return f"{self.user_name}: {self.review_date:%x}"
+class ImagenPECL2(db.Model):
+    __tablename__ = 'imagenes_pecl2'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre_fichero = db.Column(db.String(255), nullable=False)
+    pixeles_rojo = db.Column(db.Integer, nullable=False)
+    pixeles_verde = db.Column(db.Integer, nullable=False)
+    pixeles_azul = db.Column(db.Integer, nullable=False)
+    usuario = db.Column(db.String(100), nullable=False)
+    fecha_envio = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    tipo_imagen = db.Column(db.String(50))
+    ruta_imagen = db.Column(db.String(500))
